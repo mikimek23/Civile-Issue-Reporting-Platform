708f5568ee7ae77aa8e6e699fa1dede497770a10
unknown <mikimek933@gmail.com>
2025-09-29 09:49:43 +0300

    folder structure created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+//import { useState } from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Report from './pages/Report';
+import Issues from './pages/Issues';
+import Admindashboard from './pages/Admindashboard';
+function App() {
+  return (
+    <>
+    <div>
+      
+          
+         <Navbar/>
+         <BrowserRouter>
+         <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/signup' element={<Registration/>}></Route>
+          <Route path='/signin' element={<Login/>}></Route>
+          <Route path='/report' element={<Report/>}></Route>
+          <Route path='report/issues' element={<Issues/>}></Route>
+          <Route path='/admin/dashboard' element={<Admindashboard/>}></Route>
+         </Routes>
+         </BrowserRouter>
+        
+    </div>
+    </>
+  );
+}
+
+export default App;
